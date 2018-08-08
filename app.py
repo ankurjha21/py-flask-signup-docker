cac7dd52369ac851a43efce213223809d8ba1b62
Ankur jha <ankurjha19@gmail.com>
2018-08-08 16:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return " Hi Ankur Flask inside Docker !!"
+    return " Hi Ankur Flask inside Docker  Wellcome to techtalk !!"
 
 
 if __name__ == "__main__":
